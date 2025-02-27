7cfc693df131120c27182b8c9e1d289b9b2eed04
francivaldo4334 <francivaldodev@gmail.com>
2025-02-27 20:46:36 -0300

    questao 2

--- estoque/bin/estoque.dart
+++
@@ -1,5 +0,0 @@
-import 'package:estoque/estoque.dart' as estoque;
-
-void main(List<String> arguments) {
-  print('Hello world: ${estoque.calculate()}!');
-}
--- upload/bin/upload.dart
+++ upload/bin/upload.dart
@@ -2,13 +2,13 @@ import 'dart:io';
 List<String> fileTypes = ["pdf", "jpg", "png","docx"];
 void uploadFile() {
   print(fileTypes);
-  print("informe o tipo de arquivo ha ser enviado:")
+  print("informe o tipo de arquivo ha ser enviado:");
   String? type = stdin.readLineSync();
   if (!(fileTypes.contains(type))) {
       uploadFile();
       return;
   }
-  print("arquivo enviado com sucesso!")
+  print("arquivo enviado com sucesso!");
 }
 void main(List<String> arguments) {
   uploadFile(); 
